2a43ed0348fc39a6415048049ea16cf82ccded09
Jan van der Linde <jvdl@jvdl.me>
2025-11-30 20:36:21 +0000

    prepping for 2025D1 !

---
+++ 2025/01/d1a.py
@@ -0,0 +1,9 @@
+import os
+
+# clear terminal
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear()
+
+# read input
+lines = [l.strip() for l in open('2025/01/input.txt', 'r').readlines()]
