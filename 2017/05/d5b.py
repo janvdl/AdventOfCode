412532fe73832d60a203764f46659470103f3fe9
Jan van der Linde <jvdl@jvdl.me>
2026-06-14 19:51:02 +0100

    2017D5

---
+++ 2017/05/d5b.py
@@ -0,0 +1,23 @@
+import os
+from collections import defaultdict
+
+# clear terminal
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+#clear()
+
+# index of cursor in array
+cursor = 0
+steps = 0
+
+# read input
+lines = [int(l.strip()) for l in open('2017/05/input.txt', 'r').readlines()]
+maxCursor = len(lines)
+
+while cursor < maxCursor:
+    steps = steps + 1
+    value = lines[cursor]
+    lines[cursor] = lines[cursor] + (-1 if value >= 3 else 1)
+    cursor = cursor + value
+
+print(steps)
