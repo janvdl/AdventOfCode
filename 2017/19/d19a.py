5a51789e6789319b37bcf966fa8f7f9bcefc43c8
Jan van der Linde <jvdl@jvdl.me>
2026-09-19 22:02:22 +0100

    2017D19 skeleton

---
+++ 2017/19/d19a.py
@@ -0,0 +1,12 @@
+import os
+from collections import defaultdict
+
+# read input
+debug = True
+lines = None
+if debug:
+    lines = open('2017/19/input_sample.txt', 'r').readlines()
+else:
+    lines = open('2017/19/input.txt', 'r').readlines()
+
+print(lines)
